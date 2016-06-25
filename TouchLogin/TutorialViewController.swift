7a2d07bf55af555c489b5b63a71684df8207576d
johndoe133 <erickastljensen@gmail.com>
2016-06-25 17:46:00 -0400

    Tutorial
    changed stuff in the tutorial

--- TouchLogin/TutorialViewController.swift
+++ TouchLogin/TutorialViewController.swift
@@ -17,12 +17,18 @@ class TutorialViewController: UIViewController, BWWalkthroughViewControllerDeleg
 		let page_one = stb.instantiateViewControllerWithIdentifier("tutorialPage1")
 		let page_two = stb.instantiateViewControllerWithIdentifier("tutorialPage2")
 		let page_three = stb.instantiateViewControllerWithIdentifier("tutorialPage3")
+        let page_four = stb.instantiateViewControllerWithIdentifier("tutorialPage4")
+        let page_five = stb.instantiateViewControllerWithIdentifier("tutorialPage5")
+        let page_six = stb.instantiateViewControllerWithIdentifier("tutorialPage6")
 		
 		// Attach the pages to the master
 		walkthrough.delegate = self
 		walkthrough.addViewController(page_one)
 		walkthrough.addViewController(page_two)
 		walkthrough.addViewController(page_three)
+        walkthrough.addViewController(page_four)
+        walkthrough.addViewController(page_five)
+        walkthrough.addViewController(page_six)
 		
 		self.presentViewController(walkthrough, animated: true, completion: nil)
     }
